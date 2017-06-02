236feef3459ff69d91774ef9fdf2cb3f5b413712
mikealche <mikealche@gmail.com>
2017-06-02 01:55:52 -0300

    agregar luegos

--- cmake-build-debug/CMakeFiles/runTests.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/runTests.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/jscherman/Desktop/mapache/test.cpp" "/home/jscherman/Desktop/mapache/cmake-build-debug/CMakeFiles/runTests.dir/test.cpp.o"
+  "/home/mike/algo2/mapache/test.cpp" "/home/mike/algo2/mapache/cmake-build-debug/CMakeFiles/runTests.dir/test.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
